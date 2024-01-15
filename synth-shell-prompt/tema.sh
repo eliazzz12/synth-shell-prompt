127359833243cf031c82198ffe6fe72264cacfe7
eliazzz <eliasmgo@gmail.com>
2024-01-15 20:28:35 +0100

    Codigo Terminado

--- synth-shell-prompt/tema.sh
+++ synth-shell-prompt/tema.sh
@@ -2,18 +2,78 @@
 # Script creado por Elías Martín González #
 ###########################################
 
+# Variables
+
+Directory=~/.config/synth-shell/examples
+Prefix="synth-shell-prompt."
+Suffix=".config"
+NewSearch="Y"
+
+
+# Funciones
+function searchForFile(){
+    if [ -f "$FILE" ];
+    then
+        cp $FILE .././synth-shell-prompt.config
+        NewSearch="N"
+
+    elif [ -f "${Prefix}${FILE}${Suffix}" ];
+    then
+        cp ${Prefix}${FILE}${Suffix} .././synth-shell-prompt.config
+        NewSearch="N"
+    
+    else
+        echo "Archivo no encontrado :("
+        echo "¿Buscar de nuevo?"
+        read -p "[Y/n]  " NewSearch
+    fi
+}
+function searchWithParams(){
+    if [ -f "$FILE" ];
+    then
+        cp $1 .././synth-shell-prompt.config
+        NewSearch="N"
+
+    elif [ -f "${Prefix}${FILE}${Suffix}" ];
+    then
+        cp ${Prefix}${FILE}${Suffix} .././synth-shell-prompt.config
+        NewSearch="N"
+
+    else
+        echo "Archivo no encontrado :("
+        echo "Programa finalizado"
+    fi
+}
+function choose(){
+    echo
+    echo "¿Que configuración quieres cargar?"
+    echo
+    ls
+    echo
+    echo "Puedes escribir el nombre entero u omitir el *$Prefix*"
+    echo
+    read -p "Archivo: " FILE
+    echo
+    searchForFile
+}
+
+
+
+####################################################################
+# MAIN
+
 # Situarse en el directorio de synth-shell
 
-cd "/.config/synth-shell"
+cd $Directory
 
-# Preguntar qué config usar si no se ha pasado por parámetro
+# Comprobar si se ha pasado por parámetro algo o no
 
-if [ ! -z "$1" ]
+if [ -z "$1" ]
 then
-    echo "¿Que configuración quieres cargar?"
-    ll
-    echo "Puedes escribir el nombre entero o quitar *synth-shell-config*"
-    read -p  FILE
+    while [ $NewSearch == 'Y' ] || [ $NewSearch == 'y' ]; do
+        choose
+    done
+else
+    FILE=$1
+    searchWithParams
 fi
-
-
